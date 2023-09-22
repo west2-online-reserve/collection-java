852f3b12f286c92ef7218797ca0139049f42bd1a
枭笙 <1278700966@qq.com>
2023-09-22 11:36:39 +0800

    Update EmailValidator.java

--- work2/szw6/test2Bonus/EmailValidator.java
+++ work2/szw6/test2Bonus/EmailValidator.java
@@ -3,14 +3,15 @@ package szw.test2Bonus;
 public class EmailValidator {
     public static boolean isValidEmail(String email) {
         // 使用正则表达式验证邮箱格式
-        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+      String regex ="[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+";
         return email.matches(regex);
     }
     public static void main(String[] args) {
         String email1 = "test@example.com";
         String email2 = "invalid_email";
-
+        String email3="15646485@qq.com";
         System.out.println(email1 + " is valid: " + isValidEmail(email1));
         System.out.println(email2 + " is valid: " + isValidEmail(email2));
+        System.out.println(email3 + " is valid: " + isValidEmail(email3));
     }
 }
