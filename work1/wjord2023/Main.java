aeeec77c3cdaa283ffd4177c86423f885fd0fc2e
wjord <141616391+wjord2023@users.noreply.github.com>
2023-09-27 08:37:13 +0800

    修改了上次提交时出现的restock中的错误， 调换了测试的顺序

--- work1/wjord2023/Main.java
+++ work1/wjord2023/Main.java
@@ -6,9 +6,9 @@ public class Main {
         Booth booth2 = new Booth(2, "xiaoming", 200, true);
         Booth[] booth = {booth1, booth2};
         //为西瓜摊商家赋值
-        Booth.closeBooths(booth);
         Booth.purchase(booth1,50);
         booth1.restock(booth1,120);
+        Booth.closeBooths(booth);
         //测试
 
 
