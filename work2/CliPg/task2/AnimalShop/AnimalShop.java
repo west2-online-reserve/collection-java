868b01e9bd06603e8676989164cf9054537b26ac
CliPg <775075825@qq.com>
2023-09-27 18:31:25 +0800

    add: complete work2

---
+++ work2/CliPg/task2/AnimalShop/AnimalShop.java
@@ -0,0 +1,8 @@
+package com.PeanutJava.task2;
+
+public interface AnimalShop {
+    public abstract void Buy(Animal a,int num);
+    public abstract void Serve(Customer c);
+    public abstract void isClosed();
+
+}
