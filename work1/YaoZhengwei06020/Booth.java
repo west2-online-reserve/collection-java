876c2c2ba2d875607631a90bb1b7be45f6f63789
Yao <3422765404@qq.com>
2023-09-30 00:51:19 +0800

    add: complete work1

---
+++ work1/YaoZhengwei06020/Booth.java
@@ -0,0 +1,77 @@
+public class Booth {
+    private long id;
+    private String name;
+    private int total;
+    private boolean isClosed;
+    public Booth(long id, String name, int total, boolean isClosed){
+        this.id=id;
+        this.name=name;
+        this.total=total;
+        this.isClosed=isClosed;
+    }
+    public Booth(){
+
+    }
+    public long getId() {
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getTotal(){
+        return total;
+    }
+
+    public boolean getIsClosed() {
+        return isClosed;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setIsClosed(boolean isClosed) {
+        this.isClosed = isClosed;
+    }
+
+    @Override
+    public String toString() {
+        String s="摊号："+getId()+
+                "\n摊主姓名："+getName()+
+                "\n在售西瓜数："+getTotal()+
+                "\n是否休摊整改："+getIsClosed();
+        return s;
+    }
+    public static void purchase(Booth booth, int numsOfpurchases){
+        if(numsOfpurchases<0|| booth.getIsClosed()==true||numsOfpurchases> booth.getTotal()){
+            System.out.println("购买失败。");
+        }else{
+            System.out.println("购买成功。");
+        }
+    }
+    public void restock(int numsOfpurchases){
+        if (numsOfpurchases>200){
+            System.out.println("进货失败。");
+        }else{
+            total+=numsOfpurchases;
+            System.out.println("进货成功");
+        }
+    }
+    public static void closeBooths(Booth[] booths){
+        for(int i=0;i<=booths.length-1;i++){
+            if(booths[i].isClosed==false){
+                booths[i].isClosed=true;
+            }else{
+                System.out.println(booths[i].toString());
+            }
+        }
+    }
+}
---
+++ work1/YaoZhengwei06020/TestBooth.java
@@ -0,0 +1,38 @@
+public class TestBooth {
+    public static void main(String[] args) {
+        Booth a=new Booth(10001,"1号",300,false);
+        System.out.println(a.getId());
+        System.out.println(a.getName());
+        System.out.println(a.getTotal());
+        System.out.println(a.getIsClosed());
+        a.setId(10002);
+        a.setName("0号");
+        a.setTotal(299);
+        a.setIsClosed(true);
+        System.out.println("");
+        System.out.println(a.getId());
+        System.out.println(a.getName());
+        System.out.println(a.getTotal());
+        System.out.println(a.getIsClosed());
+        System.out.println(a.toString());
+        System.out.println("");
+        Booth.purchase(a,100);
+        a.setIsClosed(false);
+        Booth.purchase(a,100);
+        Booth.purchase(a,-1);
+        a.setTotal(50);
+        Booth.purchase(a,100);
+        System.out.println("");
+        a.restock(300);
+        a.restock(100);
+        System.out.println(a.getTotal());
+        System.out.println("");
+        Booth[] booths=new Booth[3];
+        booths[0]=new Booth(1,"01号",100,false);
+        booths[1]=new Booth(2,"02号",200,true);
+        booths[2]=new Booth(3,"03号",300,false);
+        Booth.closeBooths(booths);
+        System.out.println(booths[0].getIsClosed());
+        System.out.println(booths[2].getIsClosed());
+    }
+}
