010eca1f781778db66d6abeead32bde6146b8ded
gaoxiaod <249290108@qq.com>
2023-09-30 16:30:03 +0800

    102301141 - work1

---
+++ work1/gaociao/Booth.java
@@ -0,0 +1,90 @@
+public class Booth {
+
+    private long id;
+    private String name;
+    private int total;
+    private boolean isClosed;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public boolean getisClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    @Override
+    public String toString() {
+        return "waterMelon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", total=" + total +
+                ", isClosed=" + isClosed +
+                '}';
+    }
+
+    public Booth(long id, String name, int total, boolean isClosed){
+        this.id=id;
+        this.name=name;
+        this.total=total;
+        this.isClosed=isClosed;
+    }
+    public static void purchase(Booth man,int purchase){
+        if(purchase<=0){
+            System.out.println("购买数量需大于零");
+            return;
+        }
+        if( man.getisClosed()==false){
+            System.out.println("已收摊");
+            return;
+        }
+        if(purchase> man.getTotal()){
+            System.out.println("存货不足");
+            return;
+        }
+        man.total-=purchase;
+        System.out.println("交易成功");
+    }
+    public void restock(int getmelon){
+        if(getmelon>200){
+            System.out.println("进货过多,进货失败");
+            return;
+        }
+        this.total+=getmelon;
+        System.out.println("jhcg");
+    }
+    public static void closeBooth(Booth[] booths){
+        for(int i=0;i< booths.length;i++){
+            if(booths[i].getisClosed()==false){
+                booths[i].setClosed(true);
+            }
+            else{
+
+            }
+        }
+    }
+}
