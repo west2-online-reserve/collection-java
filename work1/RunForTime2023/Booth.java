0cc72269ba3177d6cdd3ccba183d5d9ccd9d9c35
RunForTime2023 <144543042+RunForTime2023@users.noreply.github.com>
2023-10-02 16:58:27 +0800

    Add files via upload

--- work1/RunForTime2023/Booth.java
+++ work1/RunForTime2023/Booth.java
@@ -3,58 +3,80 @@ public class Booth {
     private String name;
     private int total;
     private boolean IsClosed;
-    void setId(long ID) {
-        id=ID;
+
+    void setId(long id) {
+        this.id = id;
     }
-    void setName(String NAME) {
-        name=NAME;
+
+    void setName(String name) {
+        this.name = name;
     }
-    void setTotal(int TOTAL) {
-        total=TOTAL;
+
+    void setTotal(int total) {
+        this.total = total;
     }
-    void setClosed(boolean ISCLOSED) {
-        IsClosed=ISCLOSED;
+
+    void setClosed(boolean isClosed) {
+        IsClosed = isClosed;
     }
+
     long getId() {
         return id;
     }
+
     String getName() {
         return name;
     }
+
     int getTotal() {
         return total;
     }
+
     public String toString() {
-        String Output="ID:\t\t\t\t\t\t"+Long.toString(id)+"\nName:\t\t\t\t\t"+name+"\nNumber of watermelons:\t"+Integer.toString(total)+"\nIsClosed:\t\t\t\t";
-        if(IsClosed) Output=Output+"Yes\n";
-        else Output=Output+"No\n";
-        return Output;
+        String output = "ID:\t\t\t\t\t\t" + Long.toString(id) + "\nName:\t\t\t\t\t" + name + "\nNumber of watermelons:\t" + Integer.toString(total) + "\nIsClosed:\t\t\t\t";
+        if (IsClosed) {
+            output = output + "Yes\n";
+        } else {
+            output = output + "No\n";
+        }
+        return output;
     }
-    Booth(long ID,String NAME,int TOTAL,boolean iScLOSED) {
-        id=ID;
-        name=NAME;
-        total=TOTAL;
-        IsClosed=iScLOSED;
+
+    Booth(long id, String name, int total, boolean isClosed) {
+        this.id = id;
+        this.name = name;
+        this.total = total;
+        IsClosed = isClosed;
     }
-    public static void purchase(Booth stall,int num) {
-        if(num<0||stall.IsClosed||stall.total<num)
+
+    public static void purchase(Booth stall, int num) {
+        if (num < 0 || stall.IsClosed || stall.total < num){
             System.out.println("Purchase failure!");
+        }
         else {
-            stall.total-=num;
+            stall.total -= num;
             System.out.println("Purchased successfully!");
         }
     }
+
     public void restock(int num) {
-        if(num<0||num>200) System.out.println("Fail to restock!");
+        if (num < 0 || num > 200) {
+            System.out.println("Fail to restock!");
+        }
         else {
-            total+=num;
+            total += num;
             System.out.println("Restocked successfully!");
         }
     }
+
     public static void closeBooths(Booth[] stall) {
-        for(Booth x:stall) {
-            if(x.IsClosed) System.out.println(x.toString());
-            else x.IsClosed = true;
+        for (Booth x : stall) {
+            if (x.IsClosed) {
+                System.out.println(x.toString());
+            }
+            else {
+                x.IsClosed = true;
+            }
         }
     }
 }
