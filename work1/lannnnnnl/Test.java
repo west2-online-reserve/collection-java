44754e363c6819f08d0af1225fc35ff84ec631c5
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-10-03 23:42:59 +0800

    Update Test.java

--- work1/lannnnnnl/Test.java
+++ work1/lannnnnnl/Test.java
@@ -1,10 +1,21 @@
-package west2online;
 
-public class Test {
+public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Booth B1 = new Booth(101, "java", 1000, false);
+		Booth B2 = new Booth(102, "python", 300, false);
+		Booth B3 = new Booth(103, "C++", 200, true);
+		System.out.println(B1.toString());
+		B1.purchase(B1,500);
+		B2.purchase(B2,500);
+		Booth []booths= {B1 ,B2,B3};
+		Booth.closeBooths(booths);
+		B1.restock(300);
+		B2.restock(100);
+		B3.restock(-1);
+		System.out.println(B1.toString());
 	}
 
 }
+
