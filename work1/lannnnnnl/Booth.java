b4ce514b1528ed638638b95106b2d588b70456fc
unknown <email@example.com>
2023-10-03 21:11:02 +0800

    add: complete work1

---
+++ work1/lannnnnnl/Booth.java
@@ -0,0 +1,10 @@
+package west2online;
+
+public class Booth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ work1/lannnnnnl/Test.java
@@ -0,0 +1,10 @@
+package west2online;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
