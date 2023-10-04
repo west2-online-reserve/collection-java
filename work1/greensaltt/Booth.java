bf9c1d3ddebc3dde49b56aa9167f19a377cf7b1d
greensaltt <143678524+greensaltt@users.noreply.github.com>
2023-10-04 20:46:11 +0800

    <fix>:<西瓜摊修改>

--- work1/greensaltt/Booth.java
+++ work1/greensaltt/Booth.java
@@ -65,25 +65,25 @@ public class Booth {
 
     @Override
     public String toString() {
-        System.out.println(id);
-        System.out.println(name);
-        System.out.println(total);
-        System.out.println(isClosed);
+        System.out.println("该摊位的编号为："+id);
+        System.out.println("该摊位的名字为："+name);
+        System.out.println("该摊位的在售西瓜数为："+total);
+        System.out.println("是否休摊整改："+isClosed);
         return super.toString();
     }
 
 //======================================
     //静态方法purchase
-    public static int purchase(Booth id, int buy){
-        if(buy>0 && new Booth().isClosed){
+   public static int purchase(Booth id, int buy) {
+        if (buy > 0 && id.isClosed == false) {
             //购买数大于0且西瓜摊未收摊
-            if(buy < id.getTotal()){
+            if (buy < id.getTotal()) {
                 System.out.println("交易成功");
-            }else{
+            } else {
                 //购买数大于出售量报错
-                System.out.println("交易失败" );
+                System.out.println("交易失败");
             }
-        }else{
+        } else {
             System.out.println("交易失败");
         }
         return id.getTotal();
@@ -92,18 +92,22 @@ public class Booth {
     //实例方法
     public void restock(int num){
         //对应摊位进货，num进货量
-        if(num>200){
+        if(num>200 || num<=0){
             //单次进货量不能超过200
             System.out.println("进货失败");
+        }else{
+            System.out.println("进货成功")
         }
     }
 //======================================
     //静态方法closeBooths
-    public static void closeBooths(Booth booths) {
+   public static void closeBooths(Booth[] booths) {
         //让 booths 中所有未被休 业整改的摊位歇业(将 false 变为 true)
-        if (booths.isClosed==false) {
-            booths.isClosed=true;
+        for(Booth booth :booths){
+            if(booth.isClosed()==false){
+                booth.setClosed(true);
+                booth.toString();
+            }
         }
-        System.out.println(booths.toString());
     }
 }
