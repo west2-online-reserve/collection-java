3265676e8744781b30c95e8c0b71971c2dcf283a
lenovoto <141740696+lenovoto@users.noreply.github.com>
2023-10-05 00:46:58 +0800

    Update Booth.java

--- work1/lenovoto/Booth.java
+++ work1/lenovoto/Booth.java
@@ -1,10 +1,21 @@
-package homework;
+package yuhuanyin;
 
 public class Booth {
+    public Booth() {
+    }
+
+    public Booth(String name, int total, long id, boolean isClosed) {
+        this.name = name;
+        this.total = total;
+        this.id = id;
+        this.isClosed = isClosed;
+    }
+
     private String name;
     private int total;
     private long id;
     private boolean isClosed;
+
     public String getName(){
         return this.name;
     }
@@ -17,6 +28,8 @@ public class Booth {
     public boolean getIsClosed(){
         return this.isClosed;
     }
+
+
     public void setName(String name){
         this.name=name;
     }
@@ -30,42 +43,83 @@ public class Booth {
         this.isClosed = isClosed;
     }
 
-    public Booth(int total,String name,long id,boolean isClosed) {
-        this.total = total;
-        this.name = name;
-        this.id=id;
-        this.isClosed=isClosed;
-    }
+
 
     @Override
     public String toString() {
-        return "西瓜摊"  + id + "号"+" " +"姓名" +name +" "+ "数量为" + total + " "+ "是否整改" + isClosed;
+        System.out.println("Booth{" +
+                "name='" + name + '\'' +
+                ", total=" + total +
+                ", id=" + id +
+                ", isClosed=" + isClosed +
+                '}');
+        return super.toString();
     }
 
+   
     public static void purchase(Booth booth ,int nums){
         if (nums<=0||nums>booth.total||booth.isClosed) {
             System.out.println("交易失败");
         } else {booth.total -= nums;
-        System.out.println("交易成功");}
+            System.out.println("交易成功");
+            booth.total-=nums;
+        }
         return;
     }
+    
     public void restock(int quantity) {
         if (quantity<=0||quantity>200) {
             System.out.println("进货失败");
             return;
         } else {total += quantity;
-        System.out.println("现有" + quantity + "个西瓜");}
+            System.out.println("现有" + quantity + "个西瓜");
+        }
     }
+   
     public static void closeBooths(Booth[] Booths) {
-        for(int i=0;i<Booths.length;i++) {
-            if (Booths[i].getIsClosed()) {
-                System.out.println(Booths[i]);
+        for(Booth booths:Booths) {
+            if (booths.getIsClosed()) {
+                booths.toString();
             } else {
-                Booths[i].setIsClosed(true);}
+                booths.setIsClosed(true);
+                booths.toString();
+            }
         }
     }
 
 
 
 }
+/*package yuhuanyin;
+
+public class Application {
+    public static void main(String[] args) {
+        Booth booth=new Booth();
+        booth.setId(100);
+        booth.setIsClosed(false);
+        booth.setName("西瓜摊1");
+        booth.setTotal(666);
+        booth.getId();
+        booth.getName();
+        booth.getTotal();
+        booth.getIsClosed();
+        //=======================
+        booth.toString();
+        int buy=50;
+        booth.purchase(booth,buy);
+        booth.restock(buy);
+        Booth[] booths={booth};
+        booth.closeBooths(booths);
+
+    }
+}*/
+
+
+/*Booth{name='西瓜摊1', total=666, id=100, isClosed=false}
+交易成功
+现有50个西瓜
+Booth{name='西瓜摊1', total=616, id=100, isClosed=true}*/
+
+
+
 
