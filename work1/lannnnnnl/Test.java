535aa1b8039d9dc052ecf33bbf8276bd64b2fdd9
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-10-04 20:25:11 +0800

    Update Test.java

--- work1/lannnnnnl/Test.java
+++ work1/lannnnnnl/Test.java
@@ -8,11 +8,13 @@ public class test {
 		Booth B3 = new Booth(103, "C++", 200, true);
 		System.out.println(B1.toString());
 		B1.purchase(B1,500);
-		B2.purchase(B2,500);
+		B2.purchase(B2,100);
+		B3.purchase(B3,300);
 		Booth []booths= {B1 ,B2,B3};
 		Booth.closeBooths(booths);
 		B1.restock(300);
 		B2.restock(100);
+		System.out.println(B2.toString());
 		B3.restock(-1);
 		System.out.println(B1.toString());
 	}
