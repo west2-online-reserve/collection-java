1667b045c55273f0f99d20f29cd5c31a71359c41
light <1461388647@qq.com>
2023-10-04 15:00:45 +0800

    add: complete work 1

---
+++ work1/Tardylight/Test.java
@@ -0,0 +1,33 @@
+package Wangqiangstore;
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+         //创建实例对象
+        Booth A=new Booth(01,"张三",100,false);
+        A.setId(01);
+        A.setName("张三");
+        A.setTotal(100);
+        A.setIsclosed(false);
+        Booth B=new Booth(02,"李四",200,true);
+        B.setId(02);
+        B.setName("李四");
+        B.setTotal(200);
+        B.setIsclosed(false);
+        Booth C=new Booth(03,"王五",400,false);
+        C.setId(03);
+        C.setName("王五");
+        C.setTotal(400);
+        C.setIsclosed(true);
+//        测试purchase方法
+       Booth.purchase(A,10);
+       Booth.purchase(B,500);
+       Booth.purchase(C,12);
+       //测试进货
+       A.restock(120);
+       B.restock(52);
+       C.restock(500);
+//          测试closeBooths方法
+       Booth booths[]={A,B,C};
+       Booth.closeBooths(booths);
+            }
+}
---
+++ work1/Tardylight/Wangqiangstore.java
@@ -0,0 +1,78 @@
+package Wangqiangstore;
+
+
+class Booth{
+    private long id;
+    private String name;
+    private  int total;
+    private boolean isclosed;
+
+    public void setName(String name ){
+        this.name=name;
+    }
+    public void setId(long Id){
+        this.id=Id;
+    }
+    public void setTotal(int total){
+        this.total=total;
+    }
+
+    public void setIsclosed(boolean isclosed) {
+        this.isclosed = isclosed;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getIsclosed() {
+        return isclosed;
+    }
+    public Booth(long id,String name,int total,boolean isClose){
+
+    }
+    @Override
+    public String toString() {
+        return "id:"+ getId() +"    name:"+ getName()  +"   total:"+  getTotal() +"    isclosed: "+ getIsclosed()  ;
+    }
+
+    public static void purchase(Booth name, int n){
+        if (n<=0){
+            System.out.println("购买数值错误");
+        } else if (n>name.total) {
+            System.out.println("购买数量大于在售西瓜数");
+        } else if (name.isclosed==true) {
+            System.out.println("该西瓜摊位在休摊整改");
+        }else{
+            System.out.println("恭喜你成功购买了 " + n +"个西瓜");
+        }
+    }
+    public void restock(int num){
+        if(num>200){
+            System.out.println("进货数量过多导致进货失败");
+        }else {
+            System.out.println("进货成功！！！");
+        }
+    }
+    public static void closeBooths(Booth[] booths){
+        int len= booths.length;
+        for (int i=0;i<len;i++){
+            if (booths[i].getIsclosed()==false){
+                booths[i].setIsclosed(true);
+            }
+            System.out.println(booths[i]);
+            }
+
+        }
+
+    }
+
+
