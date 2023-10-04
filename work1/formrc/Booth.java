f5c34e05cca86bf46cc2b283dbdc3cc2d201052b
formrc <3051088856@qq.com>
2023-10-04 13:42:16 +0800

    style: 规范了代码格式

--- work1/formrc/Booth.java
+++ work1/formrc/Booth.java
@@ -7,84 +7,88 @@ public class Booth {
 
     private boolean isClosed;
 
-    public Booth(long id, String name, int total, boolean isClosed){
-        this.id=id;
+    public Booth(long id, String name, int total, boolean isClosed) {
+        this.id = id;
         this.name = name;
         this.total = total;
         this.isClosed = isClosed;
     }
-    public long gerId(){
+
+    public long getId() {
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
+
     public int getTotal() {
         return total;
     }
+
     public boolean getClosed() {
         return isClosed;
     }
-    public void setId(long id){
-        this.id=id;
+
+    public void setId(long id) {
+        this.id = id;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setName(String name) {
+        this.name = name;
     }
+
     public void setTotal(int total) {
         this.total = total;
     }
+
     public void setClosed(boolean closed) {
         isClosed = closed;
     }
-    public static void purchase(Booth stand, int num){
-        if(num==0)
-        {
+
+    public static void purchase(Booth stand, int num) {
+        if (num == 0) {
             System.out.println("购买的西瓜数必须为正数");
-            return ;
+            return;
         }
-        if(stand.getClosed())
-        {
+        if (stand.getClosed()) {
             System.out.println("商家处于休摊整改状态,无法购买");
-            return ;
+            return;
         }
-        if(num>stand.getTotal())
-        {
+        if (num > stand.getTotal()) {
             System.out.println("购买西瓜数不能大于在售西瓜数");
-            return ;
+            return;
         }
-        stand.setTotal(stand.getTotal()-num);
-        System.out.println("成功从"+stand.getName()+"处购买了"+num+"个西瓜");
+        stand.setTotal(stand.getTotal() - num);
+        System.out.println("成功从" + stand.getName() + "处购买了" + num + "个西瓜");
     }
-    public void restock(int num){
-        if(num<=0)
-        {
+
+    public void restock(int num) {
+        if (num <= 0) {
             System.out.println("进货数量必须为正数");
-            return ;
+            return;
         }
-        if(num>200)
-        {
+        if (num > 200) {
             System.out.println("单次进货量不能超过200");
-            return ;
+            return;
         }
-        this.total+=num;
-        System.out.println(this.name+"的西瓜摊成功进货了"+num+"个西瓜");
+        this.total += num;
+        System.out.println(this.name + "的西瓜摊成功进货了" + num + "个西瓜");
     }
-    public static void closeBooths(Booth[] booths){
-        for(Booth booth:booths)
-        {
-            if (!booth.getClosed())
-            {
+
+    public static void closeBooths(Booth[] booths) {
+        for (Booth booth : booths) {
+            if (!booth.getClosed()) {
                 booth.setClosed(true);
-                System.out.println(booth.getName()+"的摊位"+"已在休业整改。");
+                System.out.println(booth.getName() + "的摊位" + "已在休业整改。");
             }
         }
     }
-    public String toString(){
+
+    public String toString() {
         return "摊号：" + id + "\n" +
                 "摊主姓名：" + name + "\n" +
                 "在售西瓜数：" + total + "\n" +
                 "是否休摊整改：" + isClosed;
     }
 }
-
