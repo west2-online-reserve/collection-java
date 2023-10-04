f430fff8f2ceb77c0bd679a8719acf9abdfa0d0b
Poldroc <222200331@fzu.edu.cn>
2023-10-05 02:02:36 +0800

    fix: error merge

--- work1/szw6/text1/Booth.java
+++
@@ -1,5 +0,0 @@
-package text1;
-
-public class Booth {
-
-}
--- work2/CliPg/task2/AnimalShop/InsufficientBalanceException.java
+++
@@ -1,6 +0,0 @@
-package com.PeanutJava.task2;
-
-public class InsufficientBalanceException extends RuntimeException{
-    public InsufficientBalanceException(String message){super(message);}
-
-}
