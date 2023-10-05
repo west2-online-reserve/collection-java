1d88f3cd08306f01fba8e674b0e3efc13f906d11
Hart-Lin <2546841692@qq.com>
2023-10-05 20:55:29 +0800

    add:complete work1，又改了

--- work1/Hart-Lin/Booth.java
+++ work1/Hart-Lin/Booth.java
@@ -31,6 +31,14 @@ public class Booth {
     public void setClosed ( boolean closed){
         isClosed = closed;
     }
+
+    public Booth(long id, String name, int total, boolean isClosed) {
+        this.id = id;
+        this.name = name;
+        this.total = total;
+        this.isClosed = isClosed;
+    }
+
     @Override
     public String toString() {
         System.out.println("西瓜摊的摊号为"+getId());
@@ -39,34 +47,34 @@ public class Booth {
         System.out.println();
         return super.toString();
     }
-    public void restock(int PurchaseIn){
-        if (PurchaseIn <= 200){
-            this.total +=PurchaseIn;
+    public void restock(int purchaseIn){
+        if (purchaseIn <= 200){
+            this.total +=purchaseIn;
             System.out.println("进货成功");
         }else {
             System.out.println("货源地没有这么多的西瓜");
         }
     }
-    public  static void purchase(int quantity){
-        Booth instance  = new Booth();
-        if (quantity <= instance.getTotal() && instance.isClosed() != false){
-            instance.setTotal(instance.getTotal()-quantity);
+    public  static void purchase(Booth booth,int quantity){
+
+        if (quantity <=booth.getTotal() && booth.isClosed() != false){
+            booth.setTotal(booth.getTotal()-quantity);
             System.out.println("购买成功");
-        }else if(quantity <= instance.getTotal() && instance.isClosed() == false){
+        }else if(quantity <= booth.getTotal() && booth.isClosed() == false){
             System.out.println("购买失败，商家未在营业中");
-        }else if(quantity > instance.getTotal() && instance.isClosed() != false){
+        }else if(quantity > booth.getTotal() && booth.isClosed() != false){
             System.out.println("购买失败，商家西瓜数量没这么多");
         }else {
             System.out.println("购买失败，商家西瓜没这么多且商家未在营业中");
         }
     }
-    public static void closeBooth(){
-        Booth instance = new Booth();
-        if(instance.isClosed()== false){
-            System.out.println(instance.toString());
-        }else {
-            instance.setClosed(false);
-            instance.isClosed();
+    public static void closeBooths(Booth[] booths) {
+        for (int i = 0; i < booths.length; i++) {
+            if (booths[i].isClosed()) {
+                System.out.println(booths[i]);
+            } else {
+                booths[i].setClosed(true);
+            }
         }
     }
 }
