0050d4533e47cca67f48589d3e8e04d96c0217ed
tianyu-star <1109163419@qq.com>
2023-10-09 15:05:56 +0800

    add: complete work2

--- work2/tianyu-star/MyAnimalShop.java
+++ work2/tianyu-star/MyAnimalShop.java
@@ -3,55 +3,58 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 interface AnimalShop{
-    void BuyAnimal(Animal animal,MyAnimalShop shop);
-    void ServiceForCustomer(Customer customer,MyAnimalShop shop);
-    void StopShopping();
+    void buyAnimal(Animal animal, MyAnimalShop shop);
+    void serviceForCustomer(Customer customer, MyAnimalShop shop);
+    void stopShopping();
 }
 
 
 
+/**
+ * @author xuqianxun
+ */
 public class MyAnimalShop implements AnimalShop{
-    private double LeftMoney;
+    private double leftMoney;
     private boolean isShopping;
-    ArrayList<Animal> AnimalList=new ArrayList<>();
-    ArrayList<Customer> CustomerList=new ArrayList<>();
-    public MyAnimalShop(double LeftMoney,boolean isShopping){
-        this.LeftMoney=LeftMoney;
+    ArrayList<Animal> animalList =new ArrayList<>();
+    ArrayList<Customer> customerList =new ArrayList<>();
+    public MyAnimalShop(double leftMoney,boolean isShopping){
+        this.leftMoney =leftMoney;
         this.isShopping=isShopping;
     }
 
     @Override
     public String toString() {
         return "MyAnimalShop{" +
-                "LeftMoney=" + LeftMoney +
+                "LeftMoney=" + leftMoney +
                 ", isShopping=" + isShopping +
-                ", AnimalList=" + AnimalList +
-                ", CustomerList=" + CustomerList +
+                ", AnimalList=" + animalList +
+                ", CustomerList=" + customerList +
                 '}';
     }
 
     public ArrayList<Animal> getAnimalList() {
-        return AnimalList;
+        return animalList;
     }
 
     public void setAnimalList(ArrayList<Animal> animalList) {
-        AnimalList = animalList;
+        this.animalList = animalList;
     }
 
     public ArrayList<Customer> getCustomerList() {
-        return CustomerList;
+        return customerList;
     }
 
     public void setCustomerList(ArrayList<Customer> customerList) {
-        CustomerList = customerList;
+        this.customerList = customerList;
     }
 
     public double getLeftMoney() {
-        return LeftMoney;
+        return leftMoney;
     }
 
     public void setLeftMoney(double leftMoney) {
-        LeftMoney = leftMoney;
+        this.leftMoney = leftMoney;
     }
 
     public boolean isShopping() {
@@ -63,40 +66,40 @@ public class MyAnimalShop implements AnimalShop{
     }
 
     @Override
-    public void BuyAnimal(Animal animal,MyAnimalShop shop) {//买入动物
-        if (shop.LeftMoney<animal.price) {
+    public void buyAnimal(Animal animal, MyAnimalShop shop) {//买入动物
+        if (shop.leftMoney <animal.price) {
             throw new InsufficientBalanceException("Such a poor guy don't deserve this pet");
         } else {
-            shop.LeftMoney=shop.LeftMoney-animal.price;
-            AnimalList.add(animal);
+            shop.leftMoney =shop.leftMoney -animal.price;
+            animalList.add(animal);
             System.out.println("You got the perfect animal");
         }
 
     }
 
     @Override
-    public void ServiceForCustomer(Customer customer,MyAnimalShop shop) {
-        CustomerList.add(customer);
-        if (AnimalList.size()==0){
+    public void serviceForCustomer(Customer customer, MyAnimalShop shop) {
+        customerList.add(customer);
+        if (animalList.isEmpty()){
             throw new AnimalNotFountException("there's no animal for you");
         } else {
-            for (Animal animal:AnimalList){
+            for (Animal animal: animalList){
                 System.out.println(animal.toString());//遍历list输出动物信息
             }
             System.out.println("please input the number of the animal you want");
             Scanner scanner=new Scanner(System.in);
             int num=scanner.nextInt();
-            shop.LeftMoney=shop.LeftMoney+AnimalList.get(num).price+100;
-            AnimalList.remove(num);
+            shop.leftMoney =shop.leftMoney + animalList.get(num).price+100;
+            animalList.remove(num);
             customer.ArrivingTime=LocalDate.now();
         }
 
     }
 
     @Override
-    public void StopShopping() {
+    public void stopShopping() {
             int a=0;
-            for (Customer customer:CustomerList){
+            for (Customer customer: customerList){
                 if (customer.ArrivingTime.equals(LocalDate.now())){
                     System.out.println(customer.toString());
                     a=a+100;//因为设定中每个顾客买的动物利润都是100
