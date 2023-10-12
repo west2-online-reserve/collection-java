f01d33819619691cc059e59c1c9cf07841b209d2
treasonkry <1161707513@qq.com>
2023-10-12 23:43:53 +0800

    add: complete work2

--- work2/treasonkry/Dog.java
+++ work2/treasonkry/Dog.java
@@ -8,16 +8,7 @@ public class Dog extends Animal{
     public void setVaccineInjected(boolean vaccineInjected) {
         this.isVaccineInjected = vaccineInjected;
     }
-
-    public double getPrice(){
-        return 100;
-    }
-
-
-
-
-
-
+    
     public String toString(){
         return "Dog:"+"\n"+"name:"+name+"\n"+"age:"+age+"\n"+"sex"+sex+"\n"+"price:100"+"\n"+"isVaccineInjected:"+isVaccineInjected;
     }
