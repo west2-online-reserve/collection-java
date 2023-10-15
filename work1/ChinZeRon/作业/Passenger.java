99ca12788422d5d4962301cee5496c555d2c09c5
ChinZeRon <2576883361@qq.com>
2023-10-15 22:03:43 +0800

    add: complete work1

--- work1/ChinZeRon/作业/Passenger.java
+++ work1/ChinZeRon/作业/Passenger.java
@@ -2,15 +2,27 @@ package homework;
 public class Passenger {
     public static void main(String[] args) {
         Passenger passenger = new Passenger();
-        Booth booth = new Booth(102301438, "陈泽荣", 200, false);
+        Booth booth = new Booth(102301438, "陈泽荣", 20, false);
         Booth booth1 = new Booth(1, "陈", 20, false);
-        Booth booth2 = new Booth(2, "泽", 20, true);
+        Booth booth2 = new Booth(2, "泽", 20, false);
         Booth booth3 = new Booth(3, "荣", 20, true);
         Booth nums1[] = new Booth[]{booth,booth1,booth2,booth3};//测试数据，创建数组
-        booth.restock(booth,200);//西瓜摊进货
-        passenger.purchase(booth, 20);//顾客购买
-        passenger.closebooths(nums1);//全部休业整顿
+        //西瓜摊进货、顾客购买测试1：booth 正常情况：进货和购买都能够顺利完成；
+        booth.restock(booth,200);
+        passenger.purchase(booth, 20);
+        //西瓜摊进货、顾客购买测试2：booth1 进货量>200的情况；
+        booth.restock(booth1,250);
+        passenger.purchase(booth1, 20);
+        //西瓜摊进货、顾客购买测试3：booth2 库存量<顾客购买量的情况；
+        booth.restock(booth2,20);
+        passenger.purchase(booth2, 60);
+        //西瓜摊进货、顾客购买测试4：booth3 顾客购买时店铺正处于休业状态；
+        booth.restock(booth3,200);
+        passenger.purchase(booth3, 20);
+        //全部休业整顿情况测试：
+        booth.closebooths(nums1);
     }
+
     //purchase方法
     public static void purchase(Booth name, int purchaseTotal) {
         if (purchaseTotal <= 0) {
@@ -27,17 +39,6 @@ public class Passenger {
                     System.out.println("恭喜您！您的交易成功！");
                     System.out.println("店铺剩余的西瓜数量为：" + name.getTotal());
                 }
-            }
-        }
-    }
-    //closebooths方法
-    public static void closebooths(Booth[] booths){
-        for (int i = 0; i < booths.length; i++) {
-            if(booths[i].getIsClosed() == false){
-                booths[i].setIsClosed(true);
-            }else {
-                String string = booths[i].toString();
-                System.out.println(string);
             }
         }
     }
