e1e12d5f3153b723b8a100f0d065db7084204434
XiaoxiongMeng <117177149+XiaoxiongMeng@users.noreply.github.com>
2023-10-19 20:54:20 +0800

    Update CuteCats.java

--- work2/XiaoxiomgMeng/CuteCats.java
+++ work2/XiaoxiomgMeng/CuteCats.java
@@ -4,19 +4,12 @@
 public class CuteCats extends BaseAnimal{
     private final static int PRICE = 200;
 
-    public static int sum = 0;
-
     public CuteCats(String name, int age, int sex) {
         super(name, age, sex, PRICE);
-        sum++;
     }
 
     @Override
     public String toString() {
-        return "可爱的猫咪！\n" +
-                "姓名：" + name +
-                "\n年龄：" + age +
-                "\n性别：" + (sex == 0 ? "雌性" : "雄性") +
-                "\n价格：" + price;
+        return "可爱的猫咪！\n" + super.toString();
     }
 }
