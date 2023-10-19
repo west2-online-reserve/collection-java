cce3dc18414e66cfdcf2808d0efbce78c14ed431
XiaoxiongMeng <117177149+XiaoxiongMeng@users.noreply.github.com>
2023-10-19 20:53:26 +0800

    Update ChineseFieldDog.java

--- work2/XiaoxiomgMeng/ChineseFieldDog.java
+++ work2/XiaoxiomgMeng/ChineseFieldDog.java
@@ -7,19 +7,13 @@ public class ChineseFieldDog extends BaseAnimal{
     private boolean isVaccineInjected;
     //是否注射疫苗
 
-    public static int sum = 0;
-    public ChineseFieldDog(String name, int age, int sex) {
+    public ChineseFieldDog(String name, int age, int sex,boolean isVaccineInjected) {
         super(name, age, sex, PRICE);
-        sum++;
+        this.isVaccineInjected = isVaccineInjected;
     }
 
     @Override
     public String toString() {
-        return "中华田园犬\n" +
-                "姓名：" + name +
-                "\n年龄：" + age +
-                "\n性别：" + (sex == 0 ? "雌性" : "雄性") +
-                "\n价格：" + price +
-                "\n是否注射疫苗：" + isVaccineInjected;
+        return "中华田园犬\n"+ super.toString()+"\n是否注射疫苗："+isVaccineInjected;
     }
 }
