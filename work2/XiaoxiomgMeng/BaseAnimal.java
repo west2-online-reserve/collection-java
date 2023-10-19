401aa9348ae2c18ecaa7b7d9449aebee9d89f378
XiaoxiongMeng <117177149+XiaoxiongMeng@users.noreply.github.com>
2023-10-19 20:52:32 +0800

    Update BaseAnimal.java

--- work2/XiaoxiomgMeng/BaseAnimal.java
+++ work2/XiaoxiomgMeng/BaseAnimal.java
@@ -2,19 +2,18 @@
  * @author XiaoxiongMeng
  */
 public abstract class BaseAnimal {
-    public String name;
+    private String name;
     // 姓名
 
-    public int age;
+    private int age;
     // 年龄
 
-    public int sex;
+    private int sex;
     // 1代表公 0代表母
 
-    public double price;
+    private double price;
     // 价格
 
-    public boolean isSold = false;
 
     public BaseAnimal(String name, int age, int sex, double price){
         this.name = name;
@@ -23,8 +22,17 @@ public abstract class BaseAnimal {
         this.price = price;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     @Override
-    public abstract String toString();
+    public String toString(){
+        return "姓名：" + name +
+                "\n年龄：" + age +
+                "\n性别：" + (sex == 0 ? "雌性" : "雄性") +
+                "\n价格：" + price;
+    }
     /*返回动物的所有信息*/
 
 
