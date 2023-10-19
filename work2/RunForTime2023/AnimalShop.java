9b3a04ab1807b4eee62643cd166273162bdd7cec
RunForTime2023 <144543042+RunForTime2023@users.noreply.github.com>
2023-10-19 22:54:33 +0800

    Update AnimalShop.java

--- work2/RunForTime2023/AnimalShop.java
+++ work2/RunForTime2023/AnimalShop.java
@@ -1,11 +1,15 @@
 import java.time.LocalDate;
 
 public interface AnimalShop {
+    @Override
     void openShop(LocalDate openDate);
+
+    @Override
     void buyAnimals(Animal animal);
 
+    @Override
     void receiveCustomers(Customer customer, Animal animal);
 
+    @Override
     void closeShop();
-
 }
