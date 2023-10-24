09ff308d219ecf3cd0ecfc7e57528a44851212b1
102301617 <143883142+102301617@users.noreply.github.com>
2023-10-24 21:34:19 +0800

    Update Jerry.java

--- work2/102301617/Jerry.java
+++ work2/102301617/Jerry.java
@@ -4,7 +4,7 @@
 
 public class Jerry extends Animal {
     public Jerry(String name, int age, double price, String sex) {
-        super(name, age, 1000, sex,500);
+        super(name, age, 1000, sex,500,isVaccinated,true);
     }
 
     @Override
