f9063899ed09569084ab7a2b6a11f1fd232beb81
102301617 <143883142+102301617@users.noreply.github.com>
2023-10-24 21:26:06 +0800

    Update Customer.java

--- work2/102301617/Customer.java
+++ work2/102301617/Customer.java
@@ -3,17 +3,15 @@ import java.time.LocalDate;
 /**
  * @author 102301617
  */
-
 public class Customer {
-
    protected String name;
     /**
      * visit:到店次数
      * */
-  protected int visit;
+    protected int visit;
 
     protected LocalDate time;
-    @Override
+   @Override
     public String toString() {
         return name+visit+time;
     }
