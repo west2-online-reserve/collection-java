279e3b92b6bd80b8d82e0d338f2736adaf5ff2b0
102301617 <143883142+102301617@users.noreply.github.com>
2023-10-31 21:27:14 +0800

    Update Cat.java

--- work2/102301617/Cat.java
+++ work2/102301617/Cat.java
@@ -5,18 +5,17 @@
  * */
 
 
-public class Cat extends Animal{
-    public Cat(String name, int age, double price, String sex,Boolean isVaccinated) {
-        super(name, age, 200,sex,100,isVaccinated,true);
+public class Cat extends Animal {
+    public Cat(String name, int age, double price, String sex) {
+        super(name, age, 200, sex, 100);
     }
 
     @Override
     public String toString() {
-        return "name"+name+
-                "age"+age+
-                "price"+ price+
-                "sex"+sex;
+        return "name" + name +
+                "age" + age +
+                "price" + price +
+                "sex" + sex;
 
     }
 }
-
