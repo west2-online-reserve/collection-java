7da916434acac517b6dd232e893c39f2d7868265
102301617 <143883142+102301617@users.noreply.github.com>
2023-10-31 21:26:47 +0800

    Update AnimalShop.java

--- work2/102301617/AnimalShop.java
+++ work2/102301617/AnimalShop.java
@@ -1,27 +1,27 @@
 /**
  * Some method to achieve MyAnimalShop.
  * .....
-*@author 102301617
+ *@author 102301617
  *
  */
 public interface AnimalShop {
     /**
-     * @param  animal
+     * @param animal 买入动物新动物
+     */
 
-     * 买入动物新动物*/
 
-
-     void perchase(Animal animal);
+    void perchase(Animal animal);
 
     /**
-     * @param customer
-     * 招待顾客*/
-     void entertaining(Customer customer);
+     * @param customer 招待顾客
+     */
+    void entertaining(Customer customer);
 
     /**
      * 歇业
-     * 返回顾客列表，剩余动物，利润*/
-     void close();
+     * 返回顾客列表，剩余动物，利润
+     */
+    void close();
 
 
 }
