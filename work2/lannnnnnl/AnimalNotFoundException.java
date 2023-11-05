f2aa65032c9e950758ae2faf1e0d87e34725a49f
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-11-05 10:17:39 +0800

    Update AnimalNotFoundException.java

--- work2/lannnnnnl/AnimalNotFoundException.java
+++ work2/lannnnnnl/AnimalNotFoundException.java
@@ -1,7 +1,7 @@
 package westwork2;
 
 //自定义未找到动物异常
-class AnimalNotFoundException extends RuntimeException {
+public class AnimalNotFoundException extends RuntimeException {
  AnimalNotFoundException(String message) {
      super(message);
  }
