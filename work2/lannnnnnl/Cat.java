be1ee93fdcfafaf1001e883cfdc0aa02d9b0cb40
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-11-05 10:19:25 +0800

    Update Cat.java

--- work2/lannnnnnl/Cat.java
+++ work2/lannnnnnl/Cat.java
@@ -1,7 +1,7 @@
 package westwork2;
 
 //猫类
-class Cat extends Animal {
+public class Cat extends Animal {
  Cat(String name, int age, char gender) {
      super(name, age, gender, 200.0); // 价格固定为200元
  }
