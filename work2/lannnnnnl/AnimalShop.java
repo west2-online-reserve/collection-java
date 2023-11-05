b00e8ab476c39e325b7fa043d432e32e6f0a1b41
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-11-05 10:18:36 +0800

    Update AnimalShop.java

--- work2/lannnnnnl/AnimalShop.java
+++ work2/lannnnnnl/AnimalShop.java
@@ -1,7 +1,7 @@
 package westwork2;
 
 //宠物店接口
-interface AnimalShop {
+public interface AnimalShop {
  void buyNewAnimal(Animal animal) throws InsufficientBalanceException;
  void serveCustomer(Customer customer, String animalType) throws AnimalNotFoundException;
  void closeShop();
