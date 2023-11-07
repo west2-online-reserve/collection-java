b08f8a58c75c1911cb66cbea35ff607ceb688f6b
102301617 <143883142+102301617@users.noreply.github.com>
2023-11-07 20:41:45 +0800

    Update MyAnimalShop.java

--- work2/102301617/MyAnimalShop.java
+++ work2/102301617/MyAnimalShop.java
@@ -139,10 +139,10 @@ public class MyAnimalShop implements AnimalShop {
             if (!list.isEmpty()) {
                 costom.add(customer);
                 System.out.println("顾客购买的宠物为");
-                int numberOfBut = (int) (Math.random() * list.size());
-                Animal animal = (Animal) list.get(numberOfBut);
-                list.remove(numberOfBut);
-                balance += animal.price * numberOfBut;
+                int numberOfBuy = (int) (Math.random() * list.size());
+                Animal animal = (Animal) list.get(numberOfBuy);
+                list.remove(numberOfBuy);
+                balance += animal.price * numberOfBuy;
                 System.out.println(animal);
                 int i = 0;
                 customer.setVisit(i++);
@@ -168,7 +168,7 @@ public class MyAnimalShop implements AnimalShop {
         System.out.println(list);
         LocalDate date = LocalDate.now();
         int dayOfWeekValue = date.getDayOfWeek().getValue();
-        if (dayOfWeekValue<= 5) {
+        if (dayOfWeekValue>= 5) {
             run = false;
         }
     }
