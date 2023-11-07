8e3b1b396058a1526597137eeb071cde05d59db0
MOYU111 <74349514+MOYU111@users.noreply.github.com>
2023-11-07 22:19:23 +0800

    Update Dog.java

--- work2/MOYU111/Dog.java
+++ work2/MOYU111/Dog.java
@@ -1,17 +1,9 @@
 public class Dog extends animal {
     boolean isVaccineInjected = true;
 
-    public Dog(String n, int a, String s, double p) {
-        super(n, a, s, p);
+    public Dog(String name, int age, String sex, double price) {
+        super(name, age, sex, price);
     }
 
-    public String toString() {
-        return "名字是：" + name +
-                "\n年龄是：" + age +
-                "\n性别是：" + sex +
-                "\n价格是：" + price +
-                "\n狂犬疫苗注射情况为：" + isVaccineInjected+
-                "\n";
-    }
 
 }
