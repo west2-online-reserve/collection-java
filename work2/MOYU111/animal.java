9f9093764518cc644c6703c91bafcda5fb7f5421
MOYU111 <74349514+MOYU111@users.noreply.github.com>
2023-11-07 22:17:37 +0800

    Update animal.java

--- work2/MOYU111/animal.java
+++ work2/MOYU111/animal.java
@@ -1,9 +1,9 @@
 
 public abstract class animal {
-    protected String name;
-    protected int age;
-    protected String sex;
-    protected double price;
+     private String name;
+     private int age;
+     private String sex;
+     private double price;
 
     public animal(String n, int a, String s, double p) {
         this.name = n;
@@ -12,7 +12,44 @@ public abstract class animal {
         this.price = p;
     }
 
+    public String getName() {
+        return name;
+    }
 
-    public abstract String toString();
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public  String toString()
+    { return "名字是：" + name +
+            "\n年龄是：" + age +
+            "\n性别是：" + sex +
+            "\n价格是：" + price+
+            "\n";}
 
 }
+
