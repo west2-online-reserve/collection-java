dbdc454f660c786cac9954fcb1ebf1249d6100ac
bowbowww <118832120+bowbowww@users.noreply.github.com>
2023-11-07 21:43:38 +0800

    Update Rabbit.java

--- work2/832203215-work2/work2/Rabbit.java
+++ work2/832203215-work2/work2/Rabbit.java
@@ -3,16 +3,16 @@ package work2;
 public class Rabbit extends Animal{
 
     public Rabbit() {
-        super.price = 600;
+        super.setPrice(600);
     }
 
     public Rabbit(String name, int age, String gender, double price) {
         super(name, age, gender, price);
-        super.price = 600;
+        super.setPrice(600);
     }
 
     @Override
     public String toString() {
-        return "兔兔{" + "价格：" + price + ", 名字：" + name +  ", 年龄：" + age + ", 性别：" + gender + '}';
+        return "兔兔{" + "价格：" + getPrice() + ", 名字：" + getName() +  ", 年龄：" + getAge() + ", 性别：" + getGender() + '}';
     }
 }
