a623ff422f04e5cd7fc18cd8f9fd06a3ce139be1
firstGGbond <146355048+firstGGbond@users.noreply.github.com>
2023-11-07 11:24:52 +0800

    Update Bouns2.java

--- work2/firstGGbond/bouns/Bouns2.java
+++ work2/firstGGbond/bouns/Bouns2.java
@@ -1,17 +1,47 @@
 package bouns;
-import java.util.Scanner;
 public class Bouns2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        String [] arr1 = n.split(" ");
-        String n2 = sc.nextLine();
-        String [] arr2 = n2.split(" ");
-        test(arr1,arr2);
-    }
-    public static void test(String[]a1,String[]a2){
-        for(int i =0;i<a1.length;i++){
-            System.out.print(a1[i]+" "+a2[i]+" ");
-        }
-    }
+       int[] arr1 = {1, 3, 5, 7, 9};
+	   int[] arr2 = {2, 4, 6, 8, 10};
+
+	   interleavedOutput(arr2, arr1);
+	}
+	   public static void interleavedOutput(int[] arr1, int[] arr2) {
+	        Object lock = new Object();
+	        Thread thread1 = new Thread(() -> {
+	            synchronized (lock) {
+	                for (int i : arr1) {
+	                    System.out.print(i + " ");
+	                    try {
+	                        lock.notify(); 
+	                        lock.wait(); //保证只有一个线程运行。
+	                    } catch (InterruptedException e) {
+	                        e.printStackTrace();
+	                    }
+	                }
+	            }
+	        });
+
+	        Thread thread2 = new Thread(() -> {
+	            synchronized (lock) {
+	                for (int i : arr2) {
+	                    System.out.print(i + " ");
+	                    try {
+	                        lock.notify();
+	                        lock.wait();
+	                    } catch (InterruptedException e) {
+	                        e.printStackTrace();
+	                    }
+	                }
+	            }
+	        });
+	        thread1.start();
+	        thread2.start();
+	        try {
+	            thread1.join();
+	            thread2.join();
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+       }
 }
