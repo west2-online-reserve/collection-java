8054ab8263b25826be74d793ff8853860e585d35
bowbowww <118832120+bowbowww@users.noreply.github.com>
2023-11-07 21:43:00 +0800

    Update Dog.java

--- work2/832203215-work2/work2/Dog.java
+++ work2/832203215-work2/work2/Dog.java
@@ -4,12 +4,12 @@ public class Dog extends Animal {
     private boolean isVaccineInjected;
 
     public Dog() {
-        super.price = 100;
+        super.setPrice(100);
     }
 
     public Dog(String name, int age, String gender, double price,boolean isVaccineInjected) {
         super(name, age, gender, price);
-        super.price = 100;
+        super.setPrice(100);
         this.isVaccineInjected = isVaccineInjected;
     }
 
@@ -23,6 +23,6 @@ public class Dog extends Animal {
 
     @Override
     public String toString() {
-        return "狗狗{"  + "价格：" + price + ", 名字：" + name + ", 年龄：" + age + ", 性别：" + gender + ", 是否打了疫苗：" + isVaccineInjected+ '}';
+        return "狗狗{"  + "价格：" + getPrice() + ", 名字：" + getName() + ", 年龄：" + getAge() + ", 性别：" + getGender() + ", 是否打了疫苗：" + isVaccineInjected+ '}';
     }
 }
