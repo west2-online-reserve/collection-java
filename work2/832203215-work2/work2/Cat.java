d1cbb62512c7bd778e854d13dbf0f1cdb308b9f3
bowbowww <118832120+bowbowww@users.noreply.github.com>
2023-11-07 21:42:16 +0800

    Update Cat.java

--- work2/832203215-work2/work2/Cat.java
+++ work2/832203215-work2/work2/Cat.java
@@ -3,16 +3,16 @@ package work2;
 public class Cat extends Animal{
 
     public Cat() {
-        super.price =300;
+        super.setPrice(300);
     }
 
     public Cat(String name, int age, String gender, double price) {
         super(name, age, gender, price);
-        super.price = 200;
+        super.setPrice(200);
     }
 
     @Override
     public String toString() {
-        return "猫猫{" + "价格：" + price + ", 名字：" + name + ", 年龄：" + age + ", 性别：" + gender + '}';
+        return "猫猫{" + "价格：" + getPrice() + ", 名字：" + getName() + ", 年龄：" + getAge() + ", 性别：" + getGender() + '}';
     }
 }
