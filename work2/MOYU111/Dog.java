a558497af56d963cce57d24dba8181d1e979fb65
MOYU111 <74349514+MOYU111@users.noreply.github.com>
2023-11-07 22:58:42 +0800

    Update Dog.java

--- work2/MOYU111/Dog.java
+++ work2/MOYU111/Dog.java
@@ -1,5 +1,5 @@
 public class Dog extends animal {
-    boolean isVaccineInjected = true;
+    private boolean isVaccineInjected = true;
 
     public Dog(String name, int age, String sex, double price) {
         super(name, age, sex, price);
