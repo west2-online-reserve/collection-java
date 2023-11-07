054ed6973a0bc87f34925ae56f8009134db2fc39
102301617 <143883142+102301617@users.noreply.github.com>
2023-11-07 20:15:01 +0800

    Update ChineseGardenDog.java
    构造

--- work2/102301617/ChineseGardenDog.java
+++ work2/102301617/ChineseGardenDog.java
@@ -5,19 +5,26 @@
 
 
 public class ChineseGardenDog extends Animal {
-    Boolean isVaccinated;
+    private Boolean isVaccinated;
 
+    public Boolean getVaccinated() {
+        return isVaccinated;
+    }
 
-    public ChineseGardenDog(String name, int age, double price,String sex,Boolean isVaccinated) {
-        super(name, age, 100,sex,50,isVaccinated,true);
+    public void setVaccinated(Boolean vaccinated) {
+        isVaccinated = vaccinated;
+    }
 
+    public ChineseGardenDog(String name, int age, double price, String sex, Boolean isVaccinated) {
+        super(name, age, 100, sex, 50);
+         this.isVaccinated=isVaccinated;
     }
 
     @Override
     public String toString() {
-        return "name"+name+
-                "age"+age+
-                "price"+price+
-                "sex"+sex;
+        return "name" + name +
+                "age" + age +
+                "price" + price +
+                "sex" + sex;
     }
 }
