a01b0ea1a110997f4542c53ae3e306c509cc4293
Thinkweavey <143885431+Thinkweavey@users.noreply.github.com>
2023-11-07 22:12:25 +0800

    Update AnimalNotFountException.java
    去掉tostring

--- work2/143885431/AnimalNotFountException.java
+++ work2/143885431/AnimalNotFountException.java
@@ -1,17 +1,12 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+/**
+ * @author think
+ */
 
-public class AnimalNotFountException extends RuntimeException {
+public class AnimalNotFountException extends RuntimeException{
     public AnimalNotFountException() {
     }
-
     public AnimalNotFountException(String msg) {
         super(msg);
     }
 
-    public String toString() {
-        return "找不到宠物";
-    }
 }
