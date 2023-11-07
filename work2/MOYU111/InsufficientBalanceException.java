e8b10da472819ae5f7e68df305e829fb95dfab11
MOYU111 <74349514+MOYU111@users.noreply.github.com>
2023-11-07 22:51:41 +0800

    Update InsufficientBalanceException.java

--- work2/MOYU111/InsufficientBalanceException.java
+++ work2/MOYU111/InsufficientBalanceException.java
@@ -6,7 +6,8 @@ public class InsufficientBalanceException extends Throwable {
         this.money = money;
         this.name = name;
     }
-
+    
+    @Override
     public String toString(){
         return "商店余额为"+money+"\n不足以购买"+name+"\n";
     }
