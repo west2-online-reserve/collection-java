9f0cf36551a29044101bf6dbc9731f580ed3e97f
MOYU111 <74349514+MOYU111@users.noreply.github.com>
2023-11-07 22:18:53 +0800

    Update Cat.java

--- work2/MOYU111/Cat.java
+++ work2/MOYU111/Cat.java
@@ -1,14 +1,7 @@
 public class Cat extends animal{
-        public Cat(String n, int a, String s, double p){
-            super(n,a,s,p);
+        public Cat(String name, int age, String sex, double price){
+            super(name,age,sex,price);
         }
 
-        public String toString(){
-            return "名字是：" + name +
-                    "\n年龄是：" + age +
-                    "\n性别是：" + sex +
-                    "\n价格是：" + price+
-                    "\n";
-        }
+
     }
-
