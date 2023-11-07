f96056d60c56fd26191013af8385438ca6c1920c
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-11-07 16:43:16 +0800

    Update ChineseRuralDog.java

--- work2/lannnnnnl/ChineseRuralDog.java
+++ work2/lannnnnnl/ChineseRuralDog.java
@@ -2,7 +2,7 @@ package westwork2;
 
 //中华田园犬类
 public class ChineseRuralDog extends Animal {
- boolean isVaccineInjected;
+ private boolean isVaccineInjected;
 
  ChineseRuralDog(String name, int age, char gender, boolean isVaccineInjected) {
      super(name, age, gender, 100.0); // 价格固定为100元
