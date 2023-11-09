1fd512929f4933dd52ad32495e08e926b2c7c261
lannnnnnl <145467045+lannnnnnl@users.noreply.github.com>
2023-11-09 11:46:59 +0800

    Update MyAnimalShop.java

--- work2/lannnnnnl/MyAnimalShop.java
+++ work2/lannnnnnl/MyAnimalShop.java
@@ -24,38 +24,38 @@ public class MyAnimalShop implements AnimalShop{
         return this.profit;
     }
 
-    protected void setProfit(double profit) {
+    public void setProfit(double profit) {
         this.profit = profit;
     }
-    protected void decreaseBalance(double decrement) throws IllegalArgumentException, InsufficientBalanceException {
+    public void decreaseBalance(double decrement) throws IllegalArgumentException, InsufficientBalanceException {
         if (decrement < 0) {
             throw new IllegalArgumentException("Decrement should not be less than 0!");
         }
-        if (this.getBalance() < decrement) { //检查用户的当前余额
+        if (this.getBalance() < decrement) { 
             throw new InsufficientBalanceException(this.getBalance(), decrement);
         }
-        this.balance -= decrement;//在余额中扣除这笔钱
-    }//确保余额有效
+        this.balance -= decrement;
+    }
     @Override
     public void purchaseAnimal(Animal animal) throws InsufficientBalanceException {
         this.decreaseBalance(animal.getPrice());
-        this.setProfit(this.getProfit() - animal.getPrice());//设置新的利润
-        this.animals.add(animal);//将购买的动物添加到宠物店持有的动物列表中
+        this.setProfit(this.getProfit() - animal.getPrice());
+        this.animals.add(animal);
     }
     @Override
     public void receiveCustomer(Customer customer, Animal animal) throws AnimalNotFoundException{
         if(isClosed){
-            System.out.println("The shop is closed!");//判断是否关闭
+            System.out.println("The shop is closed!");
             return;
         }
         if(this.animals.contains(animal)) {
             this.animals.remove(animal);
-            this.setBalance(this.getBalance() + animal.getPrice());//余额
-            this.setProfit(this.getProfit() + animal.getPrice()+20);//利润
+            this.setBalance(this.getBalance() + animal.getPrice());
+            this.setProfit(this.getProfit() + animal.getPrice()+20);
             System.out.println(animal.toString());
-            customer.setLatestArrivedTime(LocalDate.now());//当前日期
-            customer.setTimes(customer.getTimes() + 1);//次数
-            if (!this.customers.contains(customer)) {//未包含该顾客
+            customer.setLatestArrivedTime(LocalDate.now());
+            customer.setTimes(customer.getTimes() + 1);
+            if (!this.customers.contains(customer)) {
                 this.customers.add(customer);
             }
         }
@@ -68,12 +68,12 @@ public class MyAnimalShop implements AnimalShop{
         if (!this.isClosed) {
             this.isClosed = true;
             for (Customer customer : this.customers) {
-                if (customer.getLatestArrivedTime().isEqual(LocalDate.now())) {//检查顾客到访时间
-                    System.out.println(customer.toString());//如果正确则打印顾客信息
+                if (customer.getLatestArrivedTime().isEqual(LocalDate.now())) {
+                    System.out.println(customer.toString());
                 }
             }
         }
-        System.out.println("The profit is "+this.getProfit());//不管关不关店都会打印利润
+        System.out.println("The profit is "+this.getProfit());
     }
     @Override
     public void reopen() {
