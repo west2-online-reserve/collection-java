42f60c21558f29cab508c0d1c60596e8fe3b8082
peikailv <peikailv@qq.com>
2023-11-12 22:25:04 +0800

    add:complete work2

--- work2/xm1lpk/MyanimalShop.java
+++ work2/xm1lpk/MyanimalShop.java
@@ -1,21 +1,32 @@
+package peikailv;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.time.LocalDate;
 
 public class MyanimalShop implements AnimalShop{
-    private Boolean isopen=true;
-    private Double num;
+    private Boolean isopen=false;
+    private Double num=100.0;
     private Double profit=10.00;
     /**
      * 店内余额
      **/
-    private List<Animal> animalArrayList = new ArrayList<>();
-    private List<Customer> customerArrayList = new ArrayList<>();
+    List<Animal> animalArrayList = new ArrayList<>();
+    List<Customer> customerArrayList = new ArrayList<>();
+
+    public void setNum(Double num) {
+        this.num = num;
+    }
+
+    public void setIsopen(Boolean isopen) {
+        this.isopen = isopen;
+    }
+
     @Override
     public void addanimal(Animal newanimal) {
         if(num< newanimal.price){
-            throw new InsufficientBalanceException("购买失败，原因：余额不足");
+            throw new InsufficientBalanceException("购买失败，原因：余额不足"+"\n"+"购买失败的动物信息："+"\n"+newanimal);
         }
         else {
             animalArrayList.add(newanimal);
@@ -24,10 +35,14 @@ public class MyanimalShop implements AnimalShop{
     }
     @Override
     public void dealCustomers(Customer customer){
-
+        if(!isopen) {
+            System.out.println("店铺已歇业，无法购买");
+            return ;
+        }
+        customerArrayList.add(customer);
         Scanner scanner = new Scanner(System.in);
         if (animalArrayList.size() == 0) {
-            throw new AnimalNotFountException("购买失败，原因：本店已无可售动物");
+            throw new AnimalNotFountException("购买失败，原因：本店已无可售动物"+"\n"+"购买失败的顾客信息："+"\n"+customer);
         }
         else {
             int temp;
@@ -36,12 +51,11 @@ public class MyanimalShop implements AnimalShop{
                 System.out.println(animalArrayList.get(temp).toString());
                 num = animalArrayList.get(temp).price + num + profit;
                 animalArrayList.remove(temp);
-                }
-                customer.setCount(customer.getCount() + 1);
-                customer.setArriveDay(LocalDate.now());
-                customerArrayList.add(customer);
             }
+            customer.setCount(customer.getCount() + 1);
+            customer.setArriveDay(LocalDate.now());
         }
+    }
     @Override
     public void Isopened(){
         isopen=false;
--- work2/xm1lpk/test.java
+++ work2/xm1lpk/test.java
@@ -1,26 +1,44 @@
-import java.util.Scanner;
+package peikailv;
+
+
 import java.time.LocalDate;
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
         MyanimalShop myanimalShop = new MyanimalShop();
         Scanner scanner = new Scanner(System.in);
+        //  设置初始余额
+        myanimalShop.setNum(160.0);
 
-        System.out.println("输入1购买dog，输入2购买cat");
-        int temp = scanner.nextInt();
-        if (temp == 1) {
-            ChineseTianyuanDog chineseTianyuanDog = new ChineseTianyuanDog("John", 3, 1, 20);
-            myanimalShop.addanimal(chineseTianyuanDog);
-        } else if (temp == 2) {
-            Cat cat = new Cat("Alice", 4, 1, 60);
-            myanimalShop.addanimal(cat);
+        try {
+            ChineseTianyuanDog chineseTianyuanDog1 = new ChineseTianyuanDog("John", 3, 1, 50);
+            myanimalShop.addanimal(chineseTianyuanDog1);
+
+            ChineseTianyuanDog chineseTianyuanDog2 = new ChineseTianyuanDog("Jack", 4, 0, 50);
+            myanimalShop.addanimal(chineseTianyuanDog2);
+
+            Cat cat1 = new Cat("Alice", 4, 1, 60);
+            myanimalShop.addanimal(cat1);
+
+            //  购买第四个动物时余额不足，抛出异常，并输出无法购买的动物的信息
+            Cat cat2 = new Cat("Tom", 5, 0, 60);
+            myanimalShop.addanimal(cat2);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage()+"\n");
         }
 
-        Customer c1 = new Customer("LeBron", 100, LocalDate.now());
-        Customer c2 = new Customer("James", 0, LocalDate.now());
-
-        myanimalShop.dealCustomers(c1);
-        myanimalShop.dealCustomers(c2);
-
-        myanimalShop.Isopened();
+        try {
+            Customer c1 = new Customer("LeBron", 100, LocalDate.now());
+            Customer c2 = new Customer("James", 0, LocalDate.now());
+            // 顾客1购买时店铺处于关闭状态，无法购买
+            myanimalShop.dealCustomers(c1);
+            //  开启店铺
+            myanimalShop.setIsopen(true);
+            myanimalShop.dealCustomers(c2);
+        } catch (AnimalNotFountException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
 }
