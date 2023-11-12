6e4fab36a7b76dc4939631f72a5746f1d693de03
dun51 <2953232527@qq.com>
2023-11-12 12:32:28 +0800

    add: complete work2

---
+++ work2/AnimalShop/java/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package collection
+
+public class InsufficientBalanceException extends RuntimeException{
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
