5cef014a617098a5be26ff75e6901d37c9222fe9
Huang <1601674593@qq.com>
2024-10-28 10:28:44 +0800

    :add:complete work1

--- work1/263511104/Customer.java
+++ work1/263511104/Customer.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 
 public class Customer {
     private String name;
-    private int NumbersOfVisit;
+    private int visitNumeber;
     private LocalDate lastVisitDate;
 
     public Customer(String name) {
         this.name = name;
-        this.NumbersOfVisit = 1;
+        this.visitNumeber = 1;
         this.lastVisitDate = LocalDate.now();
     }
 
@@ -17,9 +17,13 @@ public class Customer {
     public String toString(){
         return "work1.Customer{" +
                 "Name='" + name + '\'' +
-                ", Numbers of visit=" + NumbersOfVisit +
+                ", Numbers of visit=" + visitNumeber +
                 ", Time to shop='" + lastVisitDate + '\'' +
                 '}';
     }
 
+
+    public String getName() {
+        return name;
+    }
 }
