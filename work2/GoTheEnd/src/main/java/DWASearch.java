872bfaf538a6c7667189d113f517317370f1d6ed
GoTheEnd <yesirui297@outlook.com>
2024-12-01 19:19:40 +0800

    update: 实现了输出赛事详细信息功能

--- work2/GoTheEnd/src/main/java/DWASearch.java
+++ work2/GoTheEnd/src/main/java/DWASearch.java
@@ -14,41 +14,51 @@ public class DWASearch {
             "men 3m synchronised|men 10m platform|men 10m synchronised|women 1m springboard|" +
             "women 3m springboard|women 3m synchronised|women 10m platform|women 10m synchronised) detail");
 
-    public static void main(String @NotNull [] args) {
+    public static void main(String @NotNull [] args) throws FileNumberError{
+        if (args.length != 2){
+            throw new FileNumberError(("输入的文件错误, 应为2, 实为%d").formatted(args.length));
+        }
         String inputFile = args[0];
         String outputFile = args[1];
-        String contestType;
-        CoreModule coreModule = new CoreModule();
         try (
-                BufferedReader in = new BufferedReader(new FileReader("src\\main\\resources\\" + inputFile));
-                BufferedWriter out = new BufferedWriter(new FileWriter("src\\main\\resources\\" + outputFile))
+                BufferedReader in = new BufferedReader(new FileReader("src\\main\\data\\" + inputFile));
+                BufferedWriter out = new BufferedWriter(new FileWriter("src\\main\\data\\" + outputFile))
         ) {
             String inputText;
-            while ((inputText = in.readLine()) != null){
-                Matcher contestMatcher = CONTEST_REGEX.matcher(inputText);
-                Matcher playerMatcher = PLAYER_REGEX.matcher(inputText);
-                Matcher contestDetailMatcher = CONTEST_DETAIL_REGEX.matcher(inputText);
-                Matcher resultMatcher = RESULT_REGEX.matcher(inputText);
 
-                if (playerMatcher.matches()) {
-                    coreModule.displayAllPlayersInfo(out);
-                }else if (resultMatcher.matches()) {
-                    if (contestMatcher.matches()) {
-                        contestType = contestMatcher.group(1);
-                        coreModule.displayResultsForEachEvent(contestType, out);
-                    } else if (contestDetailMatcher.matches()) {
-                        contestType = contestDetailMatcher.group(1);
-                        coreModule.displayDetailResult(contestType, out);
-                    } else {
-                        out.write("N/A\n-----\n");
-                    }
-                } else {
-                    out.write("Error\n-----\n");
-                }
+            while ((inputText = in.readLine()) != null){
+                judgeText(inputText, out);
             }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void judgeText(String text, BufferedWriter out) throws IOException {
+        String contestType;
+        CoreModule coreModule = new CoreModule();
+
+            Matcher contestMatcher = CONTEST_REGEX.matcher(text);
+            Matcher playerMatcher = PLAYER_REGEX.matcher(text);
+            Matcher contestDetailMatcher = CONTEST_DETAIL_REGEX.matcher(text);
+            Matcher resultMatcher = RESULT_REGEX.matcher(text);
+
+            if (playerMatcher.matches()) {
+                coreModule.displayAllPlayersInfo(out);
+            }else if (resultMatcher.matches()) {
+                if (contestMatcher.matches()) {
+                    contestType = contestMatcher.group(1);
+                    coreModule.displayResultsForEachEvent(contestType, out);
+                } else if (contestDetailMatcher.matches()) {
+                    contestType = contestDetailMatcher.group(1);
+                    coreModule.displayDetailResult(contestType, out);
+                } else {
+                    out.write("N/A\n-----\n");
+                }
+            } else {
+                out.write("Error\n-----\n");
+            }
+
     }
 }
