9b0ca359cabfe87a97a4f743fa8c47fec347d33f
aozne <1260516267@qq.com>
2024-12-01 20:13:08 +0800

    add: complete work2

---
+++ work2/aozne/src/text/DWASearchTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+    class DWASearchTest {
+
+        @org.junit.jupiter.api.Test
+        void main() {
+            DWASearch.main(new String[]{"input.txt","output.txt"});
+        }
+    }
