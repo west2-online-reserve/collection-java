604251e2b72cc9bd4025e76088d96b6bb69cb4b0
asdskl <1510691363@qq.com>
2024-12-04 21:21:44 +0800

    add: complete work2

--- work2/asdskl/Work02/src/main/java/DWA_Search/DWASearch.java
+++ work2/asdskl/Work02/src/main/java/DWA_Search/DWASearch.java
@@ -16,13 +16,15 @@ public class DWASearch {
     public static final Pattern REGEX_CONTEST_DETAILED = Pattern.compile("^result (women 1m springboard|women 3m springboard|women 10m platform|women 3m synchronised|women 10m synchronised|men 1m springboard|men 3m springboard|men 10m platform|men 3m synchronised|men 10m synchronised) detail$");
     public static final Pattern REGEX_RESULT = Pattern.compile("^result.*");
 
-    public static void main(String[] args) throws IOException {
+
+
+        public static void main (String[]args) throws IOException {
         //判明命令行参数个数,不符则退出程序
         try {
-            if (args.length != 2){
-                throw new RuntimeException(("You should put two parameters"));
+            if (args.length != 2) {
+                throw new RuntimeException(("You should put two parameters."));
             }
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println(e.toString());
             return;
         }
@@ -36,25 +38,25 @@ public class DWASearch {
 
         //循环读取input文件的每一行指令,并检查指令正确性,执行对应的指令
         String line;
-        while ((line = FileIo.inputRead()) != null){
+        while ((line = FileIo.inputRead()) != null) {
             Matcher playersMatcher = REGEX_PLAYERS.matcher(line);
             Matcher resultMathcher = REGEX_RESULT.matcher(line);
             Matcher contestMathcher = REGEX_CONTEST.matcher(line);
             Matcher contestDetailedMathcher = REGEX_CONTEST_DETAILED.matcher(line);
             //匹配players指令
-            if (playersMatcher.find()){
+            if (playersMatcher.find()) {
                 core.outputPlayer();
                 continue;
             }
 
             //匹配result类型的指令
-            if (resultMathcher.find()){
+            if (resultMathcher.find()) {
 
-                if (contestMathcher.find()){
+                if (contestMathcher.find()) {
 
                     //输出单项目决赛结果
                     core.outputFinal(contestMathcher.group(1));
-                } else if (contestDetailedMathcher.find()){
+                } else if (contestDetailedMathcher.find()) {
                     //输出单项目的多种赛事结果
                     core.outputContestDetailed(contestDetailedMathcher.group(1));
                 } else {
