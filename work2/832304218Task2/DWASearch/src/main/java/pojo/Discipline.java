5c6c8dc413868708c3f0305529c9ab787392c63f
Enzo <2770536799@qq.com>
2025-01-03 10:48:28 +0800

    Task 3

---
+++ work2/832304218Task2/DWASearch/src/main/java/pojo/Discipline.java
@@ -0,0 +1,27 @@
+/*    */ package pojo;
+/*    */ 
+/*    */ public class Discipline {
+/*    */   private String DisciplineId;
+/*    */   private String DisciplineCode;
+/*    */   private String DisciplineName;
+/*    */   private String PhaseId;
+/*    */   private String PhaseCode;
+/*    */   private String PhaseName;
+/*    */   private String HeatName;
+/*    */   
+/*    */   public Discipline(String disciplineId, String disciplineCode, String disciplineName, String phaseId, String phaseCode, String phaseName, String heatName) {
+/* 13 */     this.DisciplineId = disciplineId;
+/* 14 */     this.DisciplineCode = disciplineCode;
+/* 15 */     this.DisciplineName = disciplineName;
+/* 16 */     this.PhaseId = phaseId;
+/* 17 */     this.PhaseCode = phaseCode;
+/* 18 */     this.PhaseName = phaseName;
+/* 19 */     this.HeatName = heatName;
+/*    */   }
+/*    */ }
+
+
+/* Location:              C:\Users\27705\Desktop\WestOnlineTask2-1.0-SNAPSHOT-jar-with-dependencies.jar!\com\westonline\pojo\Discipline.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
