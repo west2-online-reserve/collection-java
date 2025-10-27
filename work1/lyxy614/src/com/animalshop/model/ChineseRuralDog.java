6fb3cd1b7ac6db5e13d4b99c1206e99c32604243
lyxy614 <lyxy2762@163.com>
2025-10-27 21:10:44 +0800

    Change isVaccineInjected to private in ChineseRuralDog

--- work1/lyxy614/src/com/animalshop/model/ChineseRuralDog.java
+++ work1/lyxy614/src/com/animalshop/model/ChineseRuralDog.java
@@ -1,7 +1,7 @@
 package com.animalshop.model;
 
 public class ChineseRuralDog extends Animal {
-    protected boolean isVaccineInjected;
+    private boolean isVaccineInjected;
     public ChineseRuralDog(String name, int age, Gender sex, boolean isVaccineInjected){
         super(name, age, sex, 100);
         this.isVaccineInjected = isVaccineInjected;
@@ -11,3 +11,4 @@ public class ChineseRuralDog extends Animal {
         return String.format("中华田园犬 [名字: %s, 年龄: %d, 性别: %s, 价格: %.2f, 是否接种疫苗: %s]", name, age, sex, price, isVaccineInjected ? "是" : "否");
     }
 }
+
