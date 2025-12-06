abf87c863ee724dcb13ad7b5c5c2fda37796d168
Anna_0714 <17850134685@163.com>
2025-12-06 17:25:25 +0800

    add：complete work1(102401402)

---
+++ work2/anna2333/src/Const.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Const {
+    public static final ArrayList<String> EVENTS = new ArrayList<>();
+
+    static {
+        EVENTS.add("men 1m springboard");
+        EVENTS.add("men 3m springboard");
+        EVENTS.add("men 3m synchronised");
+        EVENTS.add("men 10m platform");
+        EVENTS.add("men 10m synchronised");
+        EVENTS.add("women 1m springboard");
+        EVENTS.add("women 3m springboard");
+        EVENTS.add("women 3m synchronised");
+        EVENTS.add("women 10m platform");
+        EVENTS.add("women 10m synchronised");
+    }
+}
