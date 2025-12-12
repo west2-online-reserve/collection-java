d4c590d397e11d4fb4f37580bc67bfe9e287f650
Anna_0714 <17850134685@163.com>
2025-12-12 10:04:55 +0800

    work01+work02

---
+++ work1/anna2333/src/Tools.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
