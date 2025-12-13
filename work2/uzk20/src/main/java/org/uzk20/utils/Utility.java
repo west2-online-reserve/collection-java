0490da69e5c41161c6a2c765c0c130275d1c9a7b
uzk20 <502472739@qq.com>
2025-12-13 20:28:25 +0800

    增加了README

---
+++ work2/uzk20/src/main/java/org/uzk20/utils/Utility.java
@@ -0,0 +1,24 @@
+package org.uzk20.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Utility {
+    public static String readJsonFile(String fileName){
+        StringBuilder jsonContent=new StringBuilder();
+        try (InputStream in = Utility.class.getClassLoader().getResourceAsStream(fileName); BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            if(in==null){
+                throw new RuntimeException("没找到"+fileName+"文件");
+            }
+            String line;
+            while((line= br.readLine())!=null){
+                jsonContent.append(line);
+            }
+        }catch (IOException e){
+            throw new RuntimeException("读取文件失败："+fileName,e);
+        }
+        return jsonContent.toString();
+    }
+}
